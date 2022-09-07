1f50586007a7e50b5372ae6416fc7a298ea87bae
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-09-07 14:20:07 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="text-5xl font-bold text-violet-500">Hello world</h1>;
+  return <h1 className="text-5xl font-bold text-violet-500">Hello </h1>;
 }
 
 export default App;
